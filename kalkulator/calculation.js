976d2e0ecbef0217619724486d5e259b5db80f9c
ahsalmatin <49363041+ahsalmatin@users.noreply.github.com>
2019-04-29 12:50:32 +0700

    Add files via upload

---
+++ kalkulator/calculation.js
@@ -0,0 +1,18 @@
+function btnPlus(){
+    document.form.textview.value +="+";
+}
+function btnDivided(){
+    document.form.textview.value +="/";
+}
+function btnTimes(){
+    document.form.textview.value +="*";
+}
+function btnMinus(){
+    document.form.textview.value +="-";
+}
+function btnDot(){
+    document.form.textview.value +=".";
+}
+function btnClear(){
+    document.form.textview.value ="";
+}
